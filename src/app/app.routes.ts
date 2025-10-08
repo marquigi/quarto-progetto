77ba7b1e00ee6fe025a31e986569e21f0a07154a
Mark Luigi <markluigi@yahoo.it>
2025-10-08 04:03:44 +0200

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AlbumsList } from './components/albums-list/albums-list';
+import { Home } from './components/home/home';
+import { PhotoList } from './components/photo-list/photo-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home
+    },
+    {
+        path: "albums",
+        component: AlbumsList
+    },
+    {
+        path: "photos",
+        component: PhotoList
+    },
+];
