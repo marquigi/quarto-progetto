f468d21813135c0ece271db532737084b0c854b3
Mark Luigi <markluigi@yahoo.it>
2025-10-08 03:44:42 +0200

    .

---
+++ src/app/components/albums-list/albums-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-list',
+  imports: [],
+  templateUrl: './albums-list.html',
+  styleUrl: './albums-list.css'
+})
+export class AlbumsList {
+
+}
