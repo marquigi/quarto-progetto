2b74801c892f9d19a3bbc3f623d1247e752786e5
Mark Luigi <markluigi@yahoo.it>
2025-10-09 20:46:02 +0200

    .

--- src/app/components/photo-list/photo-list.ts
+++ src/app/components/photo-list/photo-list.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { DataService } from '../../services/data-service';
 import { Photo } from '../../models/data';
 
@@ -13,10 +13,22 @@ export class PhotoList implements OnInit {
 
   photoService: DataService = inject(DataService);
 
+  route: ActivatedRoute = inject(ActivatedRoute)
+
   photos: Photo[] = [];
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const albumId = Number(this.route.snapshot.paramMap.get('id')); // prendi l'id dalla route
+    this.photos = await this.photoService.getPhotosById(albumId);    // carica le foto dell'album
+    console.log(this.photos);                                        // controlla in console
 
+
+    // const albumId = this.route.snapshot.paramMap.get("albumId")
+    // console.log("albumId")
+
+    // this.photoService.getPhotosById(albumId!)
+    //   .then(photo => this.photos = photo)
+    //   .catch(err => console.log(err))
   }
 
   // forse mi serve una variabile che mi tenga tutto getPhotosById ( Credo che mi ritorni)
